678ef1e0ee1cc4892e01e138386f00b2cdbcb0d6
Frysed <frysed@gmail.com>
2022-10-24 22:18:52 +0300

    Initial Commit

---
+++ SwiftHelper/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftHelper
+//
+//  Created by Alexander Grishin on 24.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
